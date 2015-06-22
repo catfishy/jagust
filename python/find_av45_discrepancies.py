e4aff4df87d8737f727b0a159f69a1569a8e457d
Andy Horng <andyhorng@Andys-MBP.dhcp.lbnl.us>
2015-06-22 10:38:24 -0700

    av45 discrepancy script

--- python/find_av45_discrepancies.py
+++ python/find_av45_discrepancies.py
@@ -23,13 +23,18 @@ def find_pet_discrepancies(local_csv, pet_meta_csv):
         new_date = datetime.strptime(row['EXAMDATE'], '%Y-%m-%d')
         local_pets[subj].append(new_date)
 
+    unequal_subj = []
     for k in meta_pets.keys():
         meta_vals = sorted(meta_pets[k])
         local_vals = sorted(local_pets[k])
         if meta_vals != local_vals:
+            unequal_subj.append(k)
             print "%s Unequal:" % k
             print "\tMETA: %s" % (meta_vals,)
             print "\tLOCAL: %s" % (local_vals,)
+
+    print unequal_subj
+
 
 
 if __name__ == "__main__":
