61be3d1239b369657ad90f058bcc5aad9a72a37a
Andy Horng <ahorng@HenryScsiMac159.dhcp.lbl.gov>
2015-08-14 16:35:08 -0700

    ignore pyc, added pca to region clustering

--- python/analyzeCentroidOutput.py
+++ python/analyzeCentroidOutput.py
@@ -1,7 +1,9 @@
 from scipy.spatial.distance import euclidean
-from scipy.cluster.vq import kmeans, whiten, vq, kmeans2
+from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+from sklearn.decomposition import PCA
 import numpy as np
+import matplotlib.pyplot as plt
 
 from utils import *
 
@@ -31,6 +33,7 @@ def parseResult(data, lut_table):
     for region_code, region in zip(regions, region_list):
         while len(region) <= 1:
             region = region[0]
+        key = lut_table.get(region_code, region_code)
         by_region[lut_table.get(region_code, region_code)] = region
 
     by_subj = {}
@@ -64,29 +67,64 @@ if __name__ == '__main__':
 
     by_region, by_subj, subjects = parseResult(data, lut_table)
 
+    # find centroid of centroids + normalize it somehow!!! (divide by original image x,y,z)
+    centroids = {}
+    for r in by_region.keys():
+        subj_centroids = []
+        for s in subjects:
+            cur_centroid = by_subj[s].get(r,{}).get('centroid', None)
+            if cur_centroid is not None:
+                subj_centroids.append(cur_centroid)
+        centroids[r] = np.mean(np.array(subj_centroids), axis=0)
+        print centroids[r]
+
     binding_distributions = []
     segments = []
+    variances = {}
+    means = {}
     for k,v in by_region.iteritems():
+        v = np.nan_to_num(v)
         binding_distributions.append(v)
         segments.append(k)
+        variances[k] = np.std(v)
+        means[k] = np.mean(v)
 
-    #obs = whiten(np.array(binding_distributions))
+    sorted_means = sorted(means.items(), key=lambda x: x[1], reverse=True)
+    #for k,v in sorted_means:
+    #    print "%s: %s +/- %s" % (k,v, variances[k])
+
+    # preprocessing input somehow????
     obs = np.array(binding_distributions)
-    
+
+    # PCA
+    pca_model = PCA(n_components=0.90, copy=True, whiten=False)
+    obs_pca = pca_model.fit_transform(obs)
+    for x in pca_model.explained_variance_ratio_:
+        print x
+    print obs_pca.shape
+
+    # Append centroids
+    appended = []
+    for i,x in enumerate(obs_pca):
+        appended.append(np.append(x, centroids[segments[i]]))
+        print appended
+
     scores = []
-    for k in range(10,32):
-        centroid, labels = kmeans2(data=obs, k=k, iter=10, minit='points')
-        print labels
+    for k in range(8,26):
+        model = KMeans(n_clusters=k, n_jobs=-1, copy_x=True)
+        labels = model.fit_predict(obs_pca)
         try:
-            score = silhouette_score(obs,labels,metric='euclidean')
+            score = silhouette_score(obs_pca,labels,metric='euclidean')
         except:
             score = np.nan
-        scores.append((k,score))
-    for s in scores:
-        print s
+        scores.append((k,score, model))
 
-    sys.exit(1)
+    #for s in scores:
+    #    print s
+    plt.plot([_[0] for _ in scores], [_[1] for _ in scores])
 
+    best = sorted(scores, key=lambda x: x[1], reverse=True)[0]
+    labels = best[2].predict(obs_pca)
     by_cluster_label = {_:[] for _ in list(set(labels))}
     for cluster, segment in zip(labels, segments):
         by_cluster_label[cluster].append(segment)
@@ -94,3 +132,5 @@ if __name__ == '__main__':
     for k,v in by_cluster_label.iteritems():
         print "Cluster %s" % k
         print "Segments: %s" % (v,)
+
+    plt.show()
