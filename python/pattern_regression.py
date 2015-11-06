12227f8fbd2519e94eaeaa47a417ce35bfb55fe7
Andy Horng <ahorng@222.10.243.131.in-addr.dhcp.lbl.gov>
2015-11-05 17:08:02 -0800

    dpgmm results

--- python/pattern_regression.py
+++ python/pattern_regression.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 import random
-from collections import Counter
+from collections import Counter, defaultdict
 from scipy.stats import f_oneway
 from __future__ import division
+import matplotlib.pyplot as plt
+import itertools
 
 from sklearn.mixture import GMM, VBGMM, DPGMM
 from sklearn.decomposition import PCA, KernelPCA
@@ -12,33 +14,82 @@ from sklearn.mixture import GMM, VBGMM, DPGMM
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import StandardScaler
 
-# read in data
-pattern_csv = '../pvcsummary_bl_patterns_and_change.csv'
-bl_csv = '../pvcsummary_bl_vs_change.json'
-raw_df = pd.read_json(bl_csv)
-raw_df.set_index('rid', inplace=True)
-df = pd.read_csv(pattern_csv)
-df.set_index('rid',inplace=True)
-
-
 def sample(data):
-    sample = [random.choice(data) for _ in xrange(len(data))]
-    return sample
+    return data[np.random.randint(0,len(data),(1,len(data)))[0]]
 
-def bootstrap_t_test(treatment, control, nboot = 1000, direction = "less"):
+def bootstrap_t_test(treatment, control, nboot = 10000):
+    treatment = np.array(treatment)
+    control = np.array(control)
     treatment_len = len(treatment)
-    control_len = len(control)
-    Z = treatment+control
     tstat = abs(np.mean(treatment)-np.mean(control))
+    # adjust mean
+    treatment = treatment - np.mean(treatment) + np.mean(control)
+    # concatenate and scramble
+    Z = np.concatenate((treatment,control))
+    np.random.shuffle(Z)
     tboot = np.zeros(nboot)
     for i in xrange(nboot):
         sboot = sample(Z)
-        #sboot = pd.DataFrame(np.array(sboot), columns=['treat', 'vals'])
-        #tboot[i] = np.mean(sboot['vals'][sboot['treat'] == 1]) - np.mean(sboot['vals'][sboot['treat'] == 0]) - tstat
         tboot[i] = np.mean(sboot[:treatment_len]) - np.mean(sboot[treatment_len:])
-    pvalue = len([_ for _ in tboot if abs(_) >= tstat])/float(nboot)
+    pvalue = np.sum(abs(tboot)>=tstat) / float(nboot)
     return pvalue
 
+def bootstrap_mean(vals, nboot=10000):
+    vals = np.array(vals)
+    tboot = np.zeros(nboot)
+    for i in xrange(nboot):
+        sboot = sample(vals)
+        tboot[i] = np.mean(sboot)
+    return np.mean(tboot)
+
+
+def gmm_sweep_alpha(components, data, covar_type='diag'):
+    # model selection for alpha by looking at lower bound on marginal likelihood, or log prob of X under model
+    alphas = [0.005, 0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70]
+    alpha_to_clusters = {}
+    for a in alphas:
+        clusters = []
+        scores = []
+        print a
+        for i in range(6):
+            g = DPGMM(n_components=components, 
+                      covariance_type=covar_type,
+                      alpha=a,
+                      tol=1e-5,
+                      n_iter=700,
+                      params='wmc', 
+                      init_params='wmc',
+                      verbose=False)
+            g.fit(data)
+            print g.converged_
+            y_ = g.predict(data)
+            score = np.mean(g.score(data))
+            scores.append(score)
+            clusters.append(len(set(y_)))
+        print scores
+        print clusters
+        alpha_to_clusters[a] = np.mean(scores)
+    return alpha_to_clusters
+
+def group_comparisons(df, groups, keys):
+    data=defaultdict(dict)
+    for group1, group2 in itertools.combinations(groups,2):
+        group1_members = df[df['membership']==group1]
+        group2_members = df[df['membership']==group2]
+        if len(group1_members) < 15 or len(group2_members) < 15:
+            continue
+        print "GRP %s (%s) vs GRP %s (%s)" % (group1, len(group1_members), group2, len(group2_members))
+        for i, key in enumerate(keys):
+            group1_vals = group1_members[key].tolist()
+            group2_vals = group2_members[key].tolist()
+            group1_boot = bootstrap_mean(group1_vals)
+            group2_boot = bootstrap_mean(group2_vals)
+            pvalue = bootstrap_t_test(group1_vals, group2_vals, nboot = 10000)
+            data[(group1,group2)][key] = (pvalue, group1_boot, group2_boot)
+            # if pvalue <= 0.05:
+            #     print "\t%s: %s" % (key, pvalue)
+            print "\t%s: %s (%s, %s)" % (key, pvalue, group1_boot, group2_boot)
+    return data
 
 # extract patterns and summary value, summary change
 pattern_keys = ['ctx-lh-caudalanteriorcingulate_bl',
@@ -121,43 +172,36 @@ result_keys = ['cortical_summary_bl',
                'cortical_summary_change',
                'diag']
 
+# read in data
+pattern_csv = '../pvcsummary_bl_patterns_and_change.csv'
+bl_csv = '../pvcsummary_bl_vs_change.json'
+raw_df = pd.read_json(bl_csv)
+raw_df.set_index('rid', inplace=True)
+df = pd.read_csv(pattern_csv)
+df.set_index('rid',inplace=True)
+
+
 pattern_df = df[pattern_keys]
 result_df = df[result_keys]
 rchange_df = df[rchange_keys]
 uptake_df = raw_df[pattern_keys]
 
-# convert to PCA space (99% variance)
+# Scale in original space
+scaler = StandardScaler().fit(pattern_df)
+pattern_df_raw = pd.DataFrame(scaler.transform(pattern_df))
+raw_keys = pattern_df_raw.columns
+pattern_df_raw['rid'] = result_df.index
+pattern_df_raw.set_index('rid', inplace=True)
+pattern_df_raw = pattern_df_raw.merge(result_df, left_index=True, right_index=True)
+
+# convert to PCA space + whiten(scale)
 pca_model = PCA(n_components=len(pattern_df.columns), copy=True, whiten=True)
 pattern_df_pca = pd.DataFrame(pca_model.fit_transform(pattern_df))
-for _ in pca_model.explained_variance_ratio_:
-    print _
-
 pattern_df_pca.set_index(pattern_df.index, inplace=True)
 pca_keys = pattern_df_pca.columns
 pattern_df_pca = pattern_df_pca.merge(result_df, left_index=True, right_index=True)
 
-
-# Leave one out X-validation
-loo = LeaveOneOut(n=len(pattern_df_pca.index))
-target = 'cortical_summary_bl'
-points = []
-for train_index, test_index in loo:
-    train_rows = pattern_df_pca.iloc[train_index]
-    test_rows = pattern_df_pca.iloc[test_index]
-    X_train = train_rows[pca_keys]
-    Y_train = train_rows[target].tolist()
-    X_test = test_rows[pca_keys]
-    Y_test = test_rows[target].tolist()
-    regressor = DecisionTreeRegressor(random_state=0)
-    regressor.fit(X_train,Y_train)
-    score = regressor.predict(X_test)
-    points.append((Y_test[0],score[0]))
-    print "%s -> %s" % (score, Y_test)
-plt.figure(1)
-plt.scatter([_[0] for _ in points],[_[1] for _ in points])
-plt.show()
-
-# convert to Kernel PCA Projection
+# convert to Kernel PCA Projection and scale
 kpca_model = KernelPCA(n_components=None,
                        kernel='rbf', 
                        fit_inverse_transform=True, 
@@ -167,7 +211,6 @@ X_kpca = kpca_model.fit_transform(pattern_df)
 X_back = kpca_model.inverse_transform(X_kpca)
 pattern_df_kpca = pd.DataFrame(X_kpca)
 kpca_keys = pattern_df_kpca.columns
-# scale
 scaler = StandardScaler().fit(pattern_df_kpca)
 pattern_df_kpca = pd.DataFrame(scaler.transform(pattern_df_kpca))
 pattern_df_kpca['rid'] = result_df.index
@@ -175,49 +218,33 @@ pattern_df_kpca.set_index('rid', inplace=True)
 pattern_df_kpca = pattern_df_kpca.merge(result_df, left_index=True, right_index=True)
 
 # GMM
-patterns_only = pattern_df_kpca[kpca_keys]
+raw_patterns_only = pattern_df_raw[raw_keys]
+raw_alpha_to_clusters = gmm_sweep_alpha(30, raw_patterns_only, covar_type='diag')
+print sorted(raw_alpha_to_clusters.items(), key=lambda x:x[1], reverse=True)
 
-# model selection for alpha by looking at lower bound on marginal likelihood, or log prob of X under model
-alphas = [0.05, 0.1, 0.5, 1, 5, 10, 50, 100, 500, 1e3, 5e3, 1e4]
-alpha_to_clusters = {}
-components = 20
-for a in alphas:
-    clusters = []
-    scores = []
-    for i in range(5):
-        g = DPGMM(n_components=components, 
-                  covariance_type='diag',
-                  alpha=a,
-                  tol=1e-3,
-                  n_iter=700,
-                  params='wmc', 
-                  init_params='wmc',
-                  verbose=False)
-        g.fit(patterns_only)
-        print g.converged_
-        y_ = g.predict(patterns_only)
-        score = np.mean(g.score(patterns_only))
-        scores.append(score)
-        clusters.append(len(set(y_)))
-    print scores
-    print clusters
-    alpha_to_clusters[a] = np.mean(scores)
+pca_patterns_only = pattern_df_pca[pca_keys]
+pca_alpha_to_clusters = gmm_sweep_alpha(20, pca_patterns_only)
+print sorted(pca_alpha_to_clusters.items(), key=lambda x:x[1], reverse=True)
 
-print sorted(alpha_to_clusters.items(), key=lambda x:x[1], reverse=True)
+kpca_patterns_only = pattern_df_kpca[kpca_keys]
+kpca_alpha_to_clusters = gmm_sweep_alpha(50, kpca_patterns_only)
+print sorted(kpca_alpha_to_clusters.items(), key=lambda x:x[1], reverse=True)
 
 # Cluster
-alpha = 100
-components = 20
+alpha = 5
+components = 30
 models_scores = {}
+patterns_only = raw_patterns_only
 for i in range(10):
     g = DPGMM(n_components=components, 
               covariance_type='diag',
               alpha=alpha,
-              tol=1e-3,
+              tol=1e-6,
               n_iter=700,
               params='wmc', 
               init_params='wmc',
               verbose=False)
+    print "Fitting: %s" % i
     g.fit(patterns_only)
     print g.converged_
     y_ = g.predict(patterns_only)
@@ -227,37 +254,96 @@ for i in range(10):
         membership[i][member] = 1
     bound = g.lower_bound(patterns_only, membership)
     score = np.mean(g.score(patterns_only))
-    models_scores[score] = g
+    models_scores[bound] = g
 
 best_score = max(models_scores.keys())
 best_model = models_scores[best_score]
 y_ = best_model.predict(patterns_only)
 probs = best_model.predict_proba(patterns_only)
-to_remove = []
-for i, (y, yprob) in enumerate(zip(y_, probs)):
-    print yprob
-    print yprob[y]
-    if yprob[y] < 0.99:
-        to_remove.append(i)
-
 for c in set(y_):
     print c
     print '\t%s' % len([_ for _ in y_ if _ == c])
 
 groups = list(set(y_))
-result_df['membership'] = y_
+result_df.loc[:,'membership'] = y_[:]
+
+# print group diagnoses
+for g in groups:
+    members = result_df[result_df['membership']==g]
+    if len(members) < 15:
+        continue
+    diags = dict(Counter(members['diag'].tolist()))
+    print "Group %s" % g
+    print "\tN: %s" % diags.get('N',0)
+    #print "\tSMC: %s" % diags.get('SMC',0)
+    print "\tEMCI: %s" % diags.get('EMCI',0)
+    print "\tLCMI: %s" % diags.get('LMCI',0)
+    print "\tAD: %s" % diags.get('AD',0)
 
 
-# anova for group difference in regional change
-rchange_members = rchange_df.merge(result_df, left_index=True, right_index=True)
-for i,rchange_key in enumerate(rchange_keys):
-    groupvals = {}
-    for g in groups:
-        members = rchange_members[rchange_members['membership']==g]
-        groupvals[g] = members[rchange_key].tolist()
-    Fvalue,pvalue = f_oneway(*groupvals.values())
-    print "%s: %s, %s" % (rchange_key, Fvalue, pvalue)
+# between group bootstrap hypothesis test for summary/regional change
+change_members = rchange_df.merge(result_df, left_index=True, right_index=True)
+change_keys = rchange_keys
+#change_keys = ['cortical_summary_change']
+change_pvalues = group_comparisons(change_members, groups, change_keys)
 
+# between group bootstrap hypothesis test for summary/regional uptake
+uptake_members = uptake_df.merge(result_df, left_index=True, right_index=True)
+uptake_keys = pattern_keys
+#uptake_keys = ['cortical_summary_bl']
+uptake_pvalues = group_comparisons(uptake_members, groups, uptake_keys)
+
+# aggregate effects
+effects = []
+keys = set(change_pvalues.keys()) & set(uptake_pvalues.keys())
+for k in keys:
+    group1, group2 = k
+    row = {'group1': group1,
+           'group2': group2}
+    uptake_res = uptake_pvalues[k]
+    change_res = change_pvalues[k]
+    for ck in uptake_keys:
+        uptake_pvalue = uptake_res[ck][0]
+        group1_uptake = uptake_res[ck][1]
+        group2_uptake = uptake_res[ck][2]
+        cur_row = {'group1_%s_uptake' % ck: group1_uptake,
+                   'group2_%s_uptake' % ck: group2_uptake,
+                   'pvalue_%s_uptake' % ck: uptake_pvalue}
+        row.update(cur_row)
+    for ck in change_keys:
+        change_pvalue = change_res[ck][0]
+        group1_change = change_res[ck][1]
+        group2_change = change_res[ck][2]
+        cur_row = {'group1_%s_change' % ck: group1_change,
+                   'group2_%s_change' % ck: group2_change,
+                   'pvalue_%s_change' % ck: change_pvalue}
+        row.update(cur_row)
+    effects.append(row)
+
+effects_df = pd.DataFrame(effects)
+columns = ['group1', 'group2']
+for ck in uptake_keys:
+    columns += ['group1_%s_uptake' % ck, 'group2_%s_uptake' % ck, 'pvalue_%s_uptake' % ck]
+
+for ck in change_keys:
+    columns += ['group1_%s_change' % ck, 'group2_%s_change' % ck, 'pvalue_%s_change' % ck]
+
+effects_df = effects_df.ix[:,columns]
+
+
+# plot against summary uptake/change values
+summary_bl_long = pd.melt(result_df, id_vars='membership',value_vars='cortical_summary_bl')
+summary_change_long = pd.melt(result_df, id_vars='membership',value_vars='cortical_summary_change')
+plt.figure()
+for g in groups:
+    members = summary_change_long[summary_change_long['membership']==g]
+    if len(members) < 15:
+        print "Cluster %s has only %s members" % (g, len(members))
+        continue
+    members['value'].plot(kind='kde', label=g, alpha=0.5)
+
+plt.legend()
+plt.show()
 
 # plot against regional change
 rchange_members = rchange_df.merge(result_df, left_index=True, right_index=True)
@@ -280,24 +366,29 @@ for i,pattern_key in enumerate(pattern_keys):
         members = long[long['membership']==g]
         if len(members) < 3:
             continue
-        members['value'].plot(kind='density', label='%s_%s' % (rchange_key, g))
+        members['value'].plot(kind='density', label='%s_%s' % (pattern_key, g))
     plt.legend()
 
 
-# plot against summary uptake/change values
-summary_bl_long = pd.melt(result_df, id_vars='membership',value_vars='cortical_summary_bl')
-summary_change_long = pd.melt(result_df, id_vars='membership',value_vars='cortical_summary_change')
-plt.figure()
-for g in groups:
-    members = summary_bl_long[summary_bl_long['membership']==g]
-    if len(members) < 3:
-        print "Cluster %s has only %s members" % (g, len(members))
-        continue
-    members['value'].plot(kind='density', label=g, alpha=0.5)
-
-plt.legend()
-plt.show()
 
 
 
-print "%s: %s" % (g.n_components, g.aic(patterns_only))
+# # Leave one out X-validation
+# loo = LeaveOneOut(n=len(pattern_df_pca.index))
+# target = 'cortical_summary_bl'
+# points = []
+# for train_index, test_index in loo:
+#     train_rows = pattern_df_pca.iloc[train_index]
+#     test_rows = pattern_df_pca.iloc[test_index]
+#     X_train = train_rows[pca_keys]
+#     Y_train = train_rows[target].tolist()
+#     X_test = test_rows[pca_keys]
+#     Y_test = test_rows[target].tolist()
+#     regressor = DecisionTreeRegressor(random_state=0)
+#     regressor.fit(X_train,Y_train)
+#     score = regressor.predict(X_test)
+#     points.append((Y_test[0],score[0]))
+#     print "%s -> %s" % (score, Y_test)
+# plt.figure(1)
+# plt.scatter([_[0] for _ in points],[_[1] for _ in points])
+# plt.show()
