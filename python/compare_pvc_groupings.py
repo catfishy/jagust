ff40f5c31ec08359e74c3306b02459a1e11bacb9
Andy Horng <andyhorng1@gmail.com>
2016-03-29 13:09:37 -0700

    delete files

--- python/compare_pvc_groupings.py
+++ python/compare_pvc_groupings.py
@@ -13,23 +13,28 @@ av1451_groupings = {'allregions': '../datasets/pvc_adni_av1451/allregions_output
                   'tauregions': '../datasets/pvc_adni_av1451/tauregions_output.csv'}
 
 
+groupings = av45_groupings
 
 rmse_residuals = pd.DataFrame()
 braak_stages = pd.DataFrame()
-for grouping_name, output_file in av1451_groupings.iteritems():
+for grouping_name, output_file in groupings.iteritems():
     df = pd.read_csv(output_file)
     for sid, rows in df.groupby('subject'):
         rmse_rows = rows[['rmse_residual','timepoint','subject']].drop_duplicates().dropna()
+        
+        # # only take baseline
+        # rmse_rows = rmse_rows[rmse_rows.timepoint=='BL']
+
         rmse_rows['grouping'] = grouping_name
         rmse_residuals = pd.concat((rmse_residuals,rmse_rows))
 
 
-for grouping_name in groupings.keys():
-    sns.distplot(rmse_residuals[rmse_residuals.grouping==grouping_name].rmse_residual,
-                 hist=True,kde=False,bins=100, label=grouping_name)
+# for grouping_name in groupings.keys():
+#     sns.distplot(rmse_residuals[rmse_residuals.grouping==grouping_name].rmse_residual,
+#                  hist=True,kde=False,bins=100, label=grouping_name)
 
-plt.legend()
-plt.show()
+# plt.legend()
+# plt.show()
 
 ax = sns.violinplot(x='grouping', y='rmse_residual', data=rmse_residuals, scale='width')
 plt.show()
